6f380a0aa06da15660aa199fc7d89d70265dcea9
Ashley Bland <ashleyjbland@gmail.com>
2018-09-17 11:47:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,29 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor
+  @@items
 
   def initialize(total)
-    @total = total
+    @total = 0
+
   end
+
+  def total
+  end
+
+  def add_item
+  end
+
+  def apply_discount
+  end
+
+  def items
+    @@items
+  end
+
+  def void_last_transaction
+    @@items.pop
+  end
+
 
 
 end
