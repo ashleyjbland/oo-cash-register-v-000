503d71732cc60574753481c46b9c30f4a9459a06
Ashley Bland <ashleyjbland@gmail.com>
2018-09-17 13:08:37 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
   end
 
   def apply_discount
-    self.total -= (self.total/@discount)
+    self.total -= (@price/@discount)
     #else
       #"There is no discount to apply."
   #  end
