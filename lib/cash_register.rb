8855c581b63cf911bc0e48cc670e4e00db10b8af
Ashley Bland <ashleyjbland@gmail.com>
2018-09-17 11:59:18 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,14 @@
 class CashRegister
-  attr_reader :total
+  attr_reader :total, :discount
   @@items
 
-  def initialize(employee_discount = 0)
+  def initialize(discount = 0)
     @total = 0
-
+    self.apply_discount
   end
 
   def total
+    @total = total
 
   end
 
@@ -16,6 +17,10 @@ class CashRegister
   end
 
   def apply_discount
+    if @discount > 0
+      @total.to_f / @discount
+    else
+      "There is no discount to apply."
   end
 
   def items
